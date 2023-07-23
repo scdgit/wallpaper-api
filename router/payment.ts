3a4bdd5350d0176ece93ac98851fa03710637f79
aikun <11093565+xiaosg2234@user.noreply.gitee.com>
2023-07-23 22:42:49 +0800

    完成支付宝的面支付功能的沙箱测试

--- router/payment.ts
+++ router/payment.ts
@@ -9,12 +9,13 @@ import { alipaySdk } from '../utils/alipay'
 
 // 发起支付宝的h5支付
 payment.post('/h5', async (req, res) => {
+   const { totalAmount } = req.body
    const orderId = uuidv4()
    const bizContent = {
       out_trade_no: orderId, // 商户订单号,64个字符以内、可包含字母、数字、下划线,且不能重复
       product_code: 'FAST_INSTANT_TRADE_PAY', // 销售产品码，与支付宝签约的产品码名称,仅支持FAST_INSTANT_TRADE_PAY
-      total_amount: '0.01', // 订单总金额，单位为元，精确到小数点后两位
-      subject: '色彩墙', // 订单标题
+      total_amount: totalAmount, // 订单总金额，单位为元，精确到小数点后两位
+      subject: '积分充值', // 订单标题
       body: '商品详情', // 订单描述
    }
    const result = alipaySdk.pageExec('alipay.trade.wap.pay', {
@@ -25,10 +26,30 @@ payment.post('/h5', async (req, res) => {
    res.json({ url: result })
 })
 
+// 发起支付宝的当面付
+payment.post('/inPerson', (req, res) => {
+   const { totalAmount } = req.body
+   const bizContent = {
+      out_trade_no: uuidv4(), // 商户订单号,64个字符以内、可包含字母、数字、下划线,且不能重复
+      total_amount: totalAmount, // 订单总金额，单位为元，精确到小数点后两位
+      subject: '积分充值', // 订单标题
+   }
+   const resultUrl = alipaySdk.pageExec('alipay.trade.precreate', {
+      method: 'GET',
+      bizContent
+   })
+   axios.get(resultUrl).then(resultData => {
+      // 返回订单信息
+      res.json(resultData.data)
+   }).catch(err => {
+      res.json({ msg: '订单创建失败', err })
+   })
+})
+
 // 解析支付订单状态
 payment.post('/queryOrder', async (req, res) => {
-   const { out_trade_no, trade_no } = req.body
-   const bizContent = { out_trade_no, trade_no }
+   const { outTradeNo, tradeNo } = req.body
+   const bizContent = { outTradeNo, tradeNo }
    const resultData = alipaySdk.pageExec('alipay.trade.query', {
       method: 'GET',
       bizContent
@@ -41,23 +62,23 @@ payment.post('/queryOrder', async (req, res) => {
       if (code === '10000') {
          switch (trade_status) {
             case 'WAIT_BUYER_PAY':
-               res.json({ msg: '交易创建，等待买家付款' })
+               res.json({ msg: '交易创建，等待买家付款', code, status: trade_status })
                break
             case 'TRADE_CLOSED':
-               res.json({ msg: '未付款交易超时关闭，或支付完成后全额退款' })
+               res.json({ msg: '未付款交易超时关闭，或支付完成后全额退款', code, status: trade_status })
                break
             case 'TRADE_SUCCESS':
-               res.json({ msg: '交易支付成功', queryData })
+               res.json({ msg: '交易支付成功', queryData, code, status: trade_status })
                break
             case 'TRADE_FINISHED':
-               res.json({ msg: '交易结束，不可退款' })
+               res.json({ msg: '交易结束，不可退款', code, status: trade_status })
                break
             default: res.end();
          }
       } else if (code === '40004') {
-         res.json({ msg: '业务处理失败' })
+         res.json({ msg: '业务处理失败', code })
       } else {
-         res.json({ msg: '请根据文档：https://opendoc.alipay.com/common/02km9f中的code码查阅相关信息！' })
+         res.json({ msg: '请根据文档：https://opendoc.alipay.com/common/02km9f中的code码查阅相关信息！', code })
       }
    } catch (err) {
       res.json({ msg: '查询失败', err })
